1f8f785c4c35cede08c2211cabeac85e36cdb08a
JpabloMF <juanpablo45671@gmail.com>
2019-05-19 20:30:46 -0500

    Search items added

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,12 @@ const hide = document.getElementById("hide");
 const cart = document.getElementById("cart");
 const like = document.getElementById("like");
 const sidemenu = document.getElementById("sidemenu");
+const preloader = document.getElementById("preloader");
+const searchinput = document.getElementById("searchinput");
+
 const btnlike = document.getElementById("btnlike");
 const btncart = document.getElementById("btncart");
 const btnmenu = document.getElementById("btnmenu");
-const preloader = document.getElementById("preloader");
 
 window.onload = () => {
   preloader.style.display = "none";
@@ -31,4 +33,16 @@ hide.addEventListener("click", () => {
 btnmenu.addEventListener("click", () => {
   sidemenu.style.transform = "translateX(0px)";
   hide.style.display = "block";
+});
+
+searchinput.addEventListener("keyup", () => {
+  const items = document.querySelectorAll(".games__cards li");
+  const values = document.querySelectorAll(".games__cards li .games__gametitle");
+  values.forEach((element,index) => {
+    if (!element.innerText.toLowerCase().includes(searchinput.value.toLowerCase())) {
+      items[index].style.display = "none";
+    } else {
+      items[index].style.display = "block";
+    }
+  });
 });
