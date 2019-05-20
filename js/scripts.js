67bdb9c1babdfd05035f01bfadd8491904e79ef9
JpabloMF <juanpablo45671@gmail.com>
2019-05-19 19:50:41 -0500

    Adding changes to header and sidemenu for responsive added

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 const hide = document.getElementById("hide");
 const cart = document.getElementById("cart");
 const like = document.getElementById("like");
+const sidemenu = document.getElementById("sidemenu");
 const btnlike = document.getElementById("btnlike");
 const btncart = document.getElementById("btncart");
+const btnmenu = document.getElementById("btnmenu");
 
 btncart.addEventListener("click", () => {
   cart.style.transform = "translateX(0px)";
@@ -14,8 +16,14 @@ btnlike.addEventListener("click", () => {
   hide.style.display = "block";
 });
 
-hide.addEventListener('click',() => {
+hide.addEventListener("click", () => {
   cart.style.transform = "translateX(250px)";
   like.style.transform = "translateX(250px)";
+  sidemenu.style.transform = "translateX(-250px)";
   hide.style.display = "none";
-})
+});
+
+btnmenu.addEventListener("click", () => {
+  sidemenu.style.transform = "translateX(0px)";
+  hide.style.display = "block";
+});
